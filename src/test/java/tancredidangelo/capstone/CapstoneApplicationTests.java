d73675a6db7ada28084de4425f7c02fe9314b7b8
Tancredi <tancredi.dangelo.22@gmail.com>
2026-07-26 10:23:41 +0200

    Initial commit

---
+++ src/test/java/tancredidangelo/capstone/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package tancredidangelo.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
