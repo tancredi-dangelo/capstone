d1c94aecf3b7f0c67d355207807cd2826ca6cb38
Tancredi <tancredi.dangelo.22@gmail.com>
2026-07-29 11:58:33 +0200

    Adds Security Configuration and JWT Tools

---
+++ src/main/java/tancredidangelo/capstone/security/JWTTools.java
@@ -0,0 +1,48 @@
+/*
+package tancredidangelo.capstone.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import tancredidangelo.capstone.entities.person.account.Account;
+import tancredidangelo.capstone.exceptions.UnauthorizedException;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class JWTTools {
+
+    private final String secret;
+
+    public JWTTools(@Value("${jwt.secret}") String secret) {
+        this.secret = secret;
+    }
+
+
+    public String generateToken(Account account) {
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()* 1000 * 60 * 60))
+                .subject(String.valueOf(account.getId()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+
+    }
+
+
+    public UUID extractId(String token) {
+        return UUID.fromString(Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parseSignedClaims(token).getPayload().getSubject());
+    }
+
+
+    public void verifyToken(String token) {
+        try {
+            Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parse(token);
+        } catch (Exception ex) {
+            throw new UnauthorizedException("Issues verifying token. Please try again.");
+        }
+    }
+}
+*/
---
+++ src/main/java/tancredidangelo/capstone/security/SecurityConfig.java
@@ -0,0 +1,64 @@
+/*
+package tancredidangelo.capstone.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@EnableWebSecurity
+@EnableMethodSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+
+        // remove Spring default Login Form
+        httpSecurity.formLogin(formLogin -> formLogin.disable());
+
+        // remove CSRF filter
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        // set session management to STATELESS
+        httpSecurity.sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // disable Spring default filter on every endpoint
+        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers("/**").permitAll());
+
+        // set CORS policy
+        httpSecurity.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
+        return httpSecurity.build();
+
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+}
+
+*/
