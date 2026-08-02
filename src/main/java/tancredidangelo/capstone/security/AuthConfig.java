e52208e211ed6df5d0671100b5cb9e42ac2e55e1
Tancredi <tancredi.dangelo.22@gmail.com>
2026-08-02 12:31:13 +0200

    Creates PostOwner authentication  functionality

--- src/main/java/tancredidangelo/capstone/security/AuthConfig.java
+++ src/main/java/tancredidangelo/capstone/security/AuthConfig.java
@@ -3,11 +3,25 @@ package tancredidangelo.capstone.security;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import tancredidangelo.capstone.entities.person.account.stack.Account;
+import tancredidangelo.capstone.entities.post.Post;
+import tancredidangelo.capstone.entities.post.PostService;
+
 
 import java.util.Objects;
 
 @Component("authConfig")
 public class AuthConfig {
+
+    /// dependency injection
+    private final PostService postService;
+
+    public AuthConfig(PostService postService) {
+        this.postService = postService;
+    }
+
+
+
+    /// METHODS
 
     public boolean isOwner(Long id, Authentication authentication) {
         if (authentication == null || !(authentication.getPrincipal() instanceof Account principal)) {
@@ -28,5 +42,19 @@ public class AuthConfig {
     public boolean isOwnerOrAdmin(Long id, Authentication authentication) {
         System.out.println("DEBUG Authorities: " + authentication.getAuthorities());
         return isAdmin(authentication) || isOwner(id, authentication);
+    }
+
+
+    public boolean isPostOwner(Long postId, Authentication authentication) {
+        if (authentication == null || !(authentication.getPrincipal() instanceof Account principal)) {
+            return false;
+        }
+        Post post = this.postService.findById(postId);
+        return post != null && post.getAuthor().getId().equals(principal.getId());
+    }
+
+
+    public boolean isPostOwnerOrAdmin(Long postId, Authentication authentication) {
+        return (isPostOwner(postId, authentication) || isAdmin(authentication));
     }
 }
