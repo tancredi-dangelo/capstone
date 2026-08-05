ebd254a19416c6c19efd33f77b0c3da7f95436fd
Tancredi <tancredi.dangelo.22@gmail.com>
2026-08-05 11:16:01 +0200

    Fix bug in Post controller: multipart_form_data_value

--- src/main/java/tancredidangelo/capstone/entities/post/PostController.java
+++ src/main/java/tancredidangelo/capstone/entities/post/PostController.java
@@ -5,10 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import tancredidangelo.capstone.entities.person.account.stack.Account;
 import tancredidangelo.capstone.entities.post.postDTO.requests.create.CreateCarouselRequestDTO;
 import tancredidangelo.capstone.entities.post.postDTO.requests.create.CreatePhotoRequestDTO;
@@ -30,6 +30,7 @@ import java.util.Objects;
 public class PostController {
 
     /// dependency injection
+
     private final PostService postService;
     private final WritingService writingService;
     private final PhotoService photoService;
@@ -44,16 +45,16 @@ public class PostController {
         this.carouselService = carouselService;
     }
 
+    // -------------------------------- GET --------------------------------
 
-    // -------------------- ENDPOINTS ------------------------------------------------------
-
-
-    // ----------------------  GET  --------------------------------------------------------------------
-
-    /// GET HOME FEED -> GET "[...](http://localhost/posts/home)" 200 OK
+    /// GET HOME FEED -> GET "/posts/home"
     @GetMapping("/home")
     @PreAuthorize("isAuthenticated()")
-    public Page<PostResponseDTO> getFeed(Authentication authentication, @RequestParam (defaultValue = "0") int page, @RequestParam (defaultValue = "10") int size) {
+    public Page<PostResponseDTO> getFeed(
+            Authentication authentication,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
         Long accountId = ((Account) Objects.requireNonNull(authentication.getPrincipal())).getId();
         LocalDateTime since = LocalDateTime.now().minusHours(24);
         Pageable pageable = PageRequest.of(page, size);
@@ -61,87 +62,112 @@ public class PostController {
         return this.postService.getFeed(accountId, since, pageable);
     }
 
-    /// GET SINGLE POST -> GET "[...](http://localhost/posts/{id})" 200 OK
+
+
+    /// GET SINGLE POST -> GET "/posts/{id}"
     @GetMapping("/{id}")
-    @PreAuthorize(("isAuthenticated()"))
+    @PreAuthorize("isAuthenticated()")
     public PostResponseDTO getPostById(@PathVariable Long id) {
         Post found = postService.findById(id);
         return ConverterPostDTO.convertToDTO(found);
     }
 
 
-    /// GET POSTS BY ACCOUNT ID (Account page)
+
+    /// GET POSTS BY ACCOUNT ID -> GET "/posts/accounts/{id}"
     @GetMapping("/accounts/{id}")
     @PreAuthorize("isAuthenticated()")
-    public Page<Post> getPostsByAccountId(Long id, @RequestParam (defaultValue = "0") int page, @RequestParam (defaultValue = "10") int size) {
+    public Page<Post> getPostsByAccountId(
+            @PathVariable Long id,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
         Pageable pageable = PageRequest.of(page, size);
         return this.postService.getPostsByAccountId(id, pageable);
     }
 
 
 
-    // --------------------  CREATE  ------------------------------------------------------------------
+    // -------------------------------- CREATE --------------------------------
 
-    // CREATE POST -> POST "[...](http://localhost/posts)" + {payload} 200 OK
-    // Post creation is managed individually with individual endpoints
 
+    /// CREATE WRITING -> POST "/posts/writings"
     @PostMapping("/writings")
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("isAuthenticated()")
-    public WritingResponseDTO createWriting(@RequestBody @Valid CreateWritingRequestDTO payload, Authentication authentication) {
+    public WritingResponseDTO createWriting(
+            @RequestBody @Valid CreateWritingRequestDTO payload,
+            Authentication authentication
+    ) {
         Account authenticatedAccount = (Account) authentication.getPrincipal();
         return this.writingService.createWriting(authenticatedAccount.getId(), payload);
     }
 
-    @PostMapping("/photos")
+
+
+    /// CREATE PHOTO -> POST "/posts/photos" (Multipart/Form-Data)
+    @PostMapping(value = "/photos", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("isAuthenticated()")
-    public PhotoResponseDTO createPhoto(@RequestBody @Valid CreatePhotoRequestDTO payload, Authentication authentication, MultipartFile file) {
+    public PhotoResponseDTO createPhoto(
+            @Valid @ModelAttribute CreatePhotoRequestDTO payload,
+            Authentication authentication
+    ) {
         Account authenticatedAccount = (Account) authentication.getPrincipal();
         return this.photoService.createPhoto(authenticatedAccount.getId(), payload);
     }
 
-    @PostMapping("/carousels")
+
+
+    /// CREATE CAROUSEL -> POST "/posts/carousels" (Multipart/Form-Data)
+    @PostMapping(value = "/carousels", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("isAuthenticated()")
-    public CarouselResponseDTO createCarousel(@RequestBody @Valid CreateCarouselRequestDTO payload, Authentication authentication) {
+    public CarouselResponseDTO createCarousel(
+            @Valid @ModelAttribute CreateCarouselRequestDTO payload,
+            Authentication authentication
+    ) {
         Account authenticatedAccount = (Account) authentication.getPrincipal();
         return this.carouselService.createCarousel(authenticatedAccount.getId(), payload);
     }
 
-    @PostMapping("/videos")
+
+
+    /// CREATE VIDEO -> POST "/posts/videos" (Multipart/Form-Data)
+    @PostMapping(value = "/videos", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("isAuthenticated()")
-    public VideoResponseDTO createVideo(@RequestBody @Valid CreateVideoRequestDTO payload, Authentication authentication) {
+    public VideoResponseDTO createVideo(
+            @Valid @ModelAttribute CreateVideoRequestDTO payload,
+            Authentication authentication
+    ) {
         Account authenticatedAccount = (Account) authentication.getPrincipal();
         return this.videoService.createVideo(authenticatedAccount.getId(), payload);
     }
 
 
 
-    // ------------------  UPDATE --------------------------------------------------------------------------
+    // -------------------------------- UPDATE --------------------------------
 
-
-    /// UPDATE POST -> PUT "[...](http://localhost/posts/{id})" + {payload} 200 OK
+    /// UPDATE POST -> PUT "/posts/{id}"
     @PutMapping("/{id}")
     @PreAuthorize("@authConfig.isPostOwnerOrAdmin(#id, authentication)")
-    public PostResponseDTO updatePostById(@PathVariable Long id, @RequestBody @Valid UpdatePostRequestDTO payload) {
+    public PostResponseDTO updatePostById(
+            @PathVariable Long id,
+            @RequestBody @Valid UpdatePostRequestDTO payload
+    ) {
         return this.postService.updateById(id, payload);
     }
 
 
 
-    // ------------------- DELETE -----------------------------------------------------------------------------
+    // -------------------------------- DELETE --------------------------------
 
-
-    /// DELETE POST -> DELETE "[...](http://localhost/posts/{id})" 204 OK
+    /// DELETE POST -> DELETE "/posts/{id}"
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("@authConfig.isPostOwnerOrAdmin(#id, authentication)")
     public void deletePostById(@PathVariable Long id) {
         postService.deleteById(id);
     }
-
-
-
 }
